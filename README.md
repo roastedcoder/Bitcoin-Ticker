46fcf1a483492d19c06c85c73d9f92fae9cb0d27
Suraj Kumar <vermasuraj412@gmail.com>
2021-10-23 22:43:42 +0530

    making api calls using node-express

---
+++ README.md
@@ -0,0 +1,13 @@
+# learn about APIs
+
+* **A**pplication **P**rogramming **I**nterface
+* set of commands, functions, protocols and objects.
+* client <-> server
+
+    client requests and server responses.
+    we use api to interact with other's server.
+
+we can require **request** to make http request.
+
+* most of the responses are in **JSON** format so we have to parse JSON to **js-object** to use.
+* we can **send** only one response, and to show multiple messages as response we use **write**.
---
+++ server.js
@@ -0,0 +1,46 @@
+const express = require("express");
+const request = require("request");
+
+const BASE_URL = "https://apiv2.bitcoinaverage.com/indices/global/ticker/";
+const app = express();
+app.use(express.urlencoded({extended: true}));
+
+
+app.get("/", function(req, res) {
+    res.sendFile(__dirname + "/index.html");
+});
+
+app.post("/", function(req, res) {
+
+    let crypto = req.body.crypto;
+    let fiat = req.body.fiat;
+
+    let url = BASE_URL + crypto + fiat;
+
+    request(url, function(err, response, body) {
+        
+        console.log(response.statusCode);
+        if(response.statusCode == 200) {
+
+            let data = JSON.parse(body); // parse JSON to js Object
+            let price = data.last.toString();
+            let currentDate = data.display_timestamp.toString();
+
+            res.write("<h1> The current date is <em>" + currentDate + "</em> </h1>");
+            res.write("<h2>The current price of " + crypto + " is <em>" + price + "</em> " + fiat + "</h2>");
+
+            // for multiple messages we use write so use write everywhere and use empty send to not be confused.
+            res.send();
+            // res.send("<h3>The current price of " + crypto + " is <em>" + price + "</em> " + fiat + "</h3>");
+        }
+        else {            
+            res.send("Sorry, this api is no longer free. So you have to refresh this page, maybe few times.");
+        }
+    });
+});
+
+
+
+app.listen(3000, function() {
+    console.log("server is live at port 3000...");
+});
